ee4046ff0fe0d05321553374387b42e9179f5661
Guru Dahiphale <gdahiphalebtdindia@gmail.com>
2025-09-20 03:24:03 +0530

    Initial commit

--- src/preprocess.py
+++ src/preprocess.py
@@ -3,6 +3,18 @@ import re
 import emoji
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+import nltk
+import os
+
+# Ensure stopwords are downloaded on server
+nltk_data_dir = os.path.join(os.path.dirname(__file__), "nltk_data")
+if not os.path.exists(nltk_data_dir):
+    os.makedirs(nltk_data_dir)
+
+nltk.download("stopwords", download_dir=nltk_data_dir)
+
+from nltk.corpus import stopwords
+english_stopwords = set(stopwords.words("english"))
 
 # English stopwords
 english_stopwords = set(stopwords.words('english'))
